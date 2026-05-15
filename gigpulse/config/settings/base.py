c442db490d2b3636b4a3a10bda3100d3316a66df
sivon-ai <sivonfernandes22@gmail.com>
2026-05-16 01:11:32 +0530

    new

--- gigpulse/config/settings/base.py
+++ gigpulse/config/settings/base.py
@@ -143,7 +143,7 @@ AUTH_USER_MODEL = "users.User"
 # CORS / CSRF
 
 CORS_ALLOWED_ORIGINS = env.list(
-    "CORS_ALLOWED_ORIGINS",
+    "https://gigpulse.duckdns.org",
     default=["http://localhost:5173", "http://127.0.0.1:5173"],
 )
 CORS_ALLOW_CREDENTIALS = True
